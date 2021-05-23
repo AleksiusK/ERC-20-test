00f6bd4f3f8284df608c7030bdd31d4ae8d730a9
AleksiusK <kurkela.aleksius@gmail.com>
2021-05-23 16:34:28 +0300

    Added the function for creating allowances

--- token.sol
+++ token.sol
@@ -40,6 +40,9 @@ contract Token {
         return true;
     }
 
-    
+    function approve(address _spender, uint256 _value) public returns(bool success) {
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+    }    
 
 }
